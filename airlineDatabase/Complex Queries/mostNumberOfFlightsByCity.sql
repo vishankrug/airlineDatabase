9a754ff50f22801a1dbeee46aee87a667370dd03
vishankrug <vishankr@uw.edu>
2022-05-31 23:04:28 -0700

    2 views

---
+++ airlineDatabase/Complex Queries/mostNumberOfFlightsByCity.sql
@@ -0,0 +1,11 @@
+-- Which city has the most flights per year (arrival)
+
+Use INFO_430_Proj_04;
+
+
+SELECT TOP 10 C.CityName, Count(F.FlightID) AS numberOfFlights
+FROM tblCITY C
+JOIN tblAIRPORT A ON A.CityID = C.CityID
+JOIN tblFLIGHT F ON F.ArrivalID = A.AirportID
+GROUP BY C.CityName
+ORDER BY numberOfFlights DESC
